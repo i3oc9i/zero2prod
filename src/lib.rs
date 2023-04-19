4cebf8f045cd031aa9a626223e20ca6c76f8987f
Ivano Coltellacci <ivano.coltellacci@gmail.con>
2023-04-19 19:42:32 +0200

    feat: healt check

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
+pub async fn run() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
